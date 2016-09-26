2030125e02e8273e8f3caae29fbbe98f68280351
Avishaan <avishaan@codeHatcher.com>
2016-09-26 12:18:52 -0700

    bump package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'codehatcher:orionjs',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Fix user creation issue in orionjs',
   // URL to the Git repository containing the source code for this package.

890f001463335d892b2be40e526aeda95a928cea
Avishaan <avishaan@codeHatcher.com>
2016-09-26 17:28:56 -0700

    update package version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'codehatcher:orionjs',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Fix user creation issue in orionjs',
   // URL to the Git repository containing the source code for this package.
